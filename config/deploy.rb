8e8f674e5171692488765d62289026e59a7a483b
SungHong-Cho <luanticbreeze@gmail.com>
2012-07-27 00:09:37 +0900

    edit deploy.rb to merge with master

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:darkzin/#{application}.git"
-set :branch, "edit_config"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
