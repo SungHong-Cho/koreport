bfb5268ec076f234363f35c859eaba122f1a245a
darkzin <darkzin@naver.com>
2012-07-27 19:08:54 +0900

    add : at purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,7 @@
 class Purchase < ActiveRecord::Base
   attr_accessible :isFeedback, :isPaid 
 
-  belongs_to :item, polymorphic => true
+  belongs_to :item, :polymorphic => true
 
   validates :item_id, :presence => true
 end
