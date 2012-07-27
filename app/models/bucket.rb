bc5bbee8c20a10b1229465bb7020a1054f9bcd46
darkzin <darkzin@naver.com>
2012-07-27 18:46:02 +0900

    remove primary key

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,7 +1,7 @@
 class Bucket < ActiveRecord::Base
   attr_accessible :report_id, :user_id
 
-  set_primary_keys :report_id, :user_id
+  # set_primary_keys :report_id, :user_id
 
   belongs_to :user
   belongs_to :report
