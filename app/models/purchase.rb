bb1473bd7681b181c3557b4b6b11aa0527a59fa0
SungHong-Cho <luanticbreeze@gmail.com>
2012-07-27 15:04:12 +0900

    delete wiered line at purchase.rb

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,10 +3,8 @@ class Purchase < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :report
-<<<<<<< HEAD
 
   validates :user_id, :report_id, :type, :presence => true
-=======
   belongs_to :package
->>>>>>> ca7a8be30038b69e92988ab688d45ce7df0cb063
+
 end
