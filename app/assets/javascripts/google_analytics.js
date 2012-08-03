74abe5e9ecdfc584ac50b52b369f533c26163481
SungHong-Cho <luanticbreeze@gmail.com>
2012-08-03 15:35:33 +0900

    edit google code

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-33384305-1']);
   _gaq.push(['_setDomainName', 'koreport.com']);
@@ -11,5 +9,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
-</script>
